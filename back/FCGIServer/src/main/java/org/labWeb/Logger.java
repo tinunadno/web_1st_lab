d35ae2593ff1528731745ccf35a933dcf9b71df8
tinunadno <121724512+tinunadno@users.noreply.github.com>
2024-09-25 23:50:20 +0300

    Add files via upload

---
+++ back/FCGIServer/src/main/java/org/labWeb/Logger.java
@@ -0,0 +1,30 @@
+package org.labWeb;
+
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class Logger {
+    public static void writeLogs(String str){
+        try {
+            BufferedWriter out = new BufferedWriter(
+                    new FileWriter("/home/studs/s409324/httpd-root/fcgi-bin/fcgi.log", true));
+            out.write(str+" "+getCurrentDate());
+            out.close();
+        }
+        catch (IOException e) {
+            logException(e);
+        }
+    }
+    public static void logException(Exception e)
+    {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        PrintStream ps = new PrintStream(baos);
+        e.printStackTrace(ps);
+        ps.close();
+        writeLogs( baos.toString()+" "+getCurrentDate());
+    }
+    public static String getCurrentDate(){
+        return  new SimpleDateFormat("dd.MM.yyyy HH:mm:ss").format(Calendar.getInstance().getTime());
+    }
+}
