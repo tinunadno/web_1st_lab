a985f7e223070f88f7ac790bebdc8769365c1559
tinunadno <121724512+tinunadno@users.noreply.github.com>
2024-09-25 23:48:23 +0300

    Update PointProcessor.java

--- back/FCGIServer/src/main/java/org/labWeb/PointProcessor.java
+++ back/FCGIServer/src/main/java/org/labWeb/PointProcessor.java
@@ -1,11 +1,19 @@
 package org.labWeb;
 
 public class PointProcessor {
-    public String processPoint(String xString, String yString, String rString){
-        int x=Integer.parseInt(xString);
-        int y=Integer.parseInt(yString);
-        int r=Integer.parseInt(rString);
+    public String processPoint(String x_, String y_, String r_){
+        try {
+            int x = Integer.parseInt(x_);
+            float y = Float.parseFloat(y_);
+            float r = Float.parseFloat(r_);
+            boolean firstFig=(y<=0 && y>=-r) && (x<=0 && x>=-r/2);
+            boolean secondFig=((x*x+y*y)<=(r*r/4)) && (x<=0);
+            boolean thirdFig=(y>=((x-r/2)*2)) && (x>=0) && (y<=0);
+            return (firstFig || secondFig ||thirdFig)+"";
+        }catch (Exception e){
+            return "failed to parse";
+        }
 
-        return "Response:" + ((x*x+y*y)<((r*r)/2) && x<0 && y>0 || y>x-r && x>0 && y<0 || -r/2<x && x<0 && y<0 && y>-r);
+
     }
 }
